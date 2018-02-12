50c812f7bc974a8c68025f3699a12c9d622259e7
Dmitry Raikov <design@aio.media>
2018-02-12 19:12:48 +0500

    final fixes for desktop

--- heart_check_sale70/app/al_invite.php
+++ heart_check_sale70/app/al_invite.php
@@ -2,9 +2,9 @@
 
     $values = array();
     parse_str($_POST['form'], $values);
-    $to = "design@aio.media";
-    $body = $values['name'];
-    $body .= $values['telephone'];
+    $to = "romanova@atlasprof.info";
+    $body .= 'Имя клиента: ' . $values['name'];
+    $body .= 'Телефон: ' . $values['telephone'];
     mail($to, $subject, $body);
 
 ?>
